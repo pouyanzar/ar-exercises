9c402d3524ae4c89b78e9988a3396871ac1d905a
pouyanzar <pouyan.zarbafian@hotmail.com>
2021-09-15 23:46:25 +0000

    add one-to-many assosiation between Employee and Store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Pouyan", last_name: "Zarbafian", hourly_rate: 80)
+@store1.employees.create(first_name: "Mike", last_name: "Anderson", hourly_rate: 50)
+@store2.employees.create(first_name: "Jessica", last_name: "Baker", hourly_rate: 70)
+@store2.employees.create(first_name: "Alex", last_name: "Morphy", hourly_rate: 60)
+@store2.employees.create(first_name: "Jane", last_name: "Pembelton", hourly_rate: 50)
